ae81f20cfe1635d2a0fa80f69dc780860d52f565
Tarun Jangra <51834686+tarunjangra4@users.noreply.github.com>
2023-09-05 17:01:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Register /> */}
+      {/* <Login /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
