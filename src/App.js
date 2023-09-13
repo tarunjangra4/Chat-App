c215a501bfdf8ea770fed612feaeb169a0551986
Tarun Jangra <51834686+tarunjangra4@users.noreply.github.com>
2023-09-13 14:20:15 +0530

    Routing added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import "./App.css";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { currUser } = useContext(AuthContext);
+  console.log(currUser);
+
+  const ProtectedRout = ({ children }) => {
+    if (!currUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route path="/" element={<Home />} />
+          {/* <Route path="" element={currUser ? <Home /> : <Login />} /> */}
+          <Route
+            path=""
+            element={
+              <ProtectedRout>
+                <Home />
+              </ProtectedRout>
+            }
+          />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Register />} />
         </Route>
